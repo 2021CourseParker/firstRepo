ee08bbab62874fb779636dfbc19b96532339b45d
Parker <dzavid.bestija@gmail.com>
2021-01-22 20:39:00 +0800

    add Program 1.1

--- Code/Code/Program.cs
+++ Code/Code/Program.cs
@@ -8,67 +8,67 @@ namespace Code
         {
 
 
-            //int total = 0, n = 0;
-            //Console.WriteLine("Choosing a number");
-            //total = Console.Read();
-            //while (true)
-            //{
-            //    if (total % 3 == 2)
-            //    {
-            //        total = total - 2;
-            //        Console.WriteLine(" I subtract 2");
+            int total = 0, n = 0;
+            Console.WriteLine("Choosing a number");
+            total = Console.Read();
+            while (true)
+            {
+                if (total % 3 == 2)
+                {
+                    total = total - 2;
+                    Console.WriteLine(" I subtract 2");
 
-            //    }
+                }
 
-            //    else
-            //    {
-            //        total--;
-            //        Console.WriteLine("I subtract 1");
-            //    }
+                else
+                {
+                    total--;
+                    Console.WriteLine("I subtract 1");
+                }
 
-            //    Console.WriteLine("Now the number is" + total);
+                Console.WriteLine("Now the number is" + total);
 
-            //    if (total <= 0)
-            //    {
-            //        Console.WriteLine("I win");
-            //        break;
+                if (total <= 0)
+                {
+                    Console.WriteLine("I win");
+                    break;
 
-            //    }
-
-
-
-
-            //    Console.WriteLine("subtract 1 or 2");
-            //    n = Console.Read();
-
-            //    while (n < 1 || n > 2)
-            //    {
-            //        Console.WriteLine("Only 1 or 2");
-            //        Console.WriteLine("Rewrite please");
-
-            //        n = Console.Read();
-
-            //    }
-
-            //    total = total - n;
-            //    Console.WriteLine("Now the number is" + total);
-            //    if (total <= 0)
-            //    {
-            //        Console.WriteLine("you win");
-            //        break;
-            //    }
+                }
 
 
 
 
-            //}
+                Console.WriteLine("subtract 1 or 2");
+                n = Console.Read();
 
-            int x;
-            x = Console.Read();
-            Console.WriteLine("d" + x);
-            int n = 0;
-            n = Console.Read();
-            Console.WriteLine("b" + n);
+                while (n < 1 || n > 2)
+                {
+                    Console.WriteLine("Only 1 or 2");
+                    Console.WriteLine("Rewrite please");
+
+                    n = Console.Read();
+
+                }
+
+                total = total - n;
+                Console.WriteLine("Now the number is" + total);
+                if (total <= 0)
+                {
+                    Console.WriteLine("you win");
+                    break;
+                }
+
+
+
+
+            }
+
+            //int x;
+            //x = Console.Read();
+            //Console.WriteLine("d" + x);
+            //int n = 0;
+            //n = Console.Read();
+            //Console.WriteLine("b" + n);
         }
     }
 }
